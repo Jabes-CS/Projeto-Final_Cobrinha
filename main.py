d196abe4416449c93f5880c068758598cbb2d5af
Your Name <jabes.candido1@gmail.com>
2024-06-04 20:05:49 -0300

    Jogo finalizado/lógica - Pronto para personalização

--- main.py
+++ main.py
@@ -4,23 +4,23 @@ import random
 
 pygame.init()
 pygame.display.set_caption("Jogo Snake Python")
-largura, altura = 1200, 750
+largura, altura = 1200, 800
 tela = pygame.display.set_mode((largura, altura))
 relogio = pygame.time.Clock() # Este relógio serve para acada troca do ponteiro do tempo a cobra anda o valor da variavel (velocidade_cobra) 
 
-# -cores RGB
+# Cores RGB
 preta = (0, 0, 0)
 branca = (255, 255, 255)
 vermelha = (255, 0, 0)
 verde = (0, 255, 0)
 
-# -parametros da cobrinha
+# Parametros da cobrinha
 tamanho_quadrado = 20
 velocidade_jogo = 15 # Podemos implementar também que quanto mais a cobra vai crescendo ela vai ficando mais rápida
 
 def gerar_cimida():
-    comida_x = round(random.randrange(0, largura - tamanho_quadrado) / 20.0) * 20.0
-    comida_y = round(random.randrange(0, altura - tamanho_quadrado) / 20.0) * 20.0
+    comida_x = round(random.randrange(0, largura - tamanho_quadrado) / float(tamanho_quadrado)) * float(tamanho_quadrado)
+    comida_y = round(random.randrange(0, altura - tamanho_quadrado) / float(tamanho_quadrado)) * float(tamanho_quadrado)
     return comida_x, comida_y
 
 def desenhar_comida(tamanho, comida_x, comida_y):
@@ -36,7 +36,19 @@ def desenhar_pontuacao(pontuacao):
     tela.blit(texto, [1, 1])
 
 def selecionar_velocidade(tecla):
-    pass
+    if tecla == pygame.K_DOWN:
+        velocidade_x = 0
+        velocidade_y = tamanho_quadrado
+    elif tecla == pygame.K_UP:
+        velocidade_x = 0
+        velocidade_y = -tamanho_quadrado
+    elif tecla == pygame.K_RIGHT:
+        velocidade_x = tamanho_quadrado
+        velocidade_y = 0
+    elif tecla == pygame.K_LEFT:
+        velocidade_x = -tamanho_quadrado
+        velocidade_y = 0
+    return velocidade_x, velocidade_y
 
 def rodar_jogo():
     fim_jogo = False
@@ -63,6 +75,13 @@ def rodar_jogo():
 
         # -desenhar_comida
         desenhar_comida(tamanho_quadrado, comida_x, comida_y)
+
+        # -atualizar a posição da cobra
+        if x < 0 or x >= largura or y < 0 or y>= altura:
+            fim_jogo = True
+
+        x += velocidade_x
+        y += velocidade_y
 
         # -desenhar_cobra
         pixels.append([x, y]) #adicionando novo pixel a cobra
